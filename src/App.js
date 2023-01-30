a0e8e05aca636a2a4b75b8bc81eda07db6d297d7
Raoof-e-Services <raoofeservices@gmail.com>
2023-01-30 13:26:06 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [post,setPost]=useState([])
+
+
+  const getPost = async() =>{
+   console.log("hello")
+  try {
+    const apipost  = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const postfromapi = await apipost.json();
+    setPost(postfromapi[0])
+    console.log(postfromapi);
+  }catch(error){
+
+  }finally{
+
+  }  
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>{post.title}</p>
+    {/* {post.map((item) =>(
+      <p>{item.title}</p>
+    ) )
+      
+    } */}
+    <button onClick={getPost}>Click to see post one</button>      
     </div>
   );
 }
